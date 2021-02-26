d9d85eb342743df0583b0e58cc481eb98185b4a9
Vladislav Petrenko <v.petrenko@room8studio.com>
2021-02-26 20:43:02 +0200

    init project

---
+++ OdysseyServer.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OdysseyServer.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ OdysseyServer.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OdysseyServer.Services
+{
+    public class Class1
+    {
+    }
+}
